25d97ac25beb965cb40a3b9777a3cfdd9c1336fc
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-10-27 22:27:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# This function returns the entirety of "Dracula" as a string
+def readBook():
+  f = open("dracula.txt", "r")
+  s = f.read().replace("-", " ")
+  f.close()
+  return ''.join(c for c in s if c.isalnum() or c == " ")
+  
